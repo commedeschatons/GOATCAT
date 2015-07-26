26ae1c3d2a62f4c2b92f225f3e980f06f17e4d16
Polina Aleks <a5@Polinas-MacBook-Pro.local>
2015-07-25 21:22:39 -0400

    Added function to run through all orders, print the addresses of awaiting orders. Fixed some international address compatibility issues

--- ship.py
+++ ship.py
@@ -6,7 +6,6 @@ from pprint import pprint
 
 authheader = {'Authorization': 'Basic MThkNmEwMDI4YzkzNDFkY2FjMGFjMzI1NzYwNjZmZGY6OTgxM2VkY2MyYTU3NGQ1NGE3N2QzZjBiNGVkNGI0OTk='}
 url_awaitingshipment = 'orders?orderStatus=awaiting_shipment'
-base = "https://ssapi.shipstation.com/"
 
 def getSSResponse(path):
     request = Request('https://ssapi.shipstation.com/' + path, headers=authheader)
@@ -16,25 +15,44 @@ def getSSResponse(path):
 class Order:
     def __init__(self, orderID):
         self.orderID = orderID
-        request = Request("https://ssapi.shipstation.com/orders/" + self.orderID, headers=authheader)
-        response_body = urlopen(request).read()
-        allinfo = json.loads(response_body)
+        allinfo = getSSResponse('orders/' + self.orderID)
         self.city = allinfo['shipTo']['city']
         self.name = allinfo['shipTo']['name']
         self.postalcode = allinfo['shipTo']['postalCode']
         self.street1 = allinfo['shipTo']['street1']
         self.street2 = allinfo['shipTo']['street2']
         self.country = allinfo['shipTo']['country']
+        self.state = allinfo['shipTo']['state']
 
     def getStreet(self):
         Streets = self.street1 +"\n"+ self.street2
         return Streets
+
     def getAddress(self):
-        Address = self.name + '\n' + self.street1 +"\n"+ self.street2+"\n"+ self.city + "\n" + self.postalcode
+        if self.street2 != "":
+            Streets = self.street1 +"\n"+ self.street2
+        else:
+            Streets = self.street1
+        if self.state != "":
+            state = ", " + self.state
+        else:
+            state = ""
+        Address = self.name + '\n' + Streets +"\n"+ self.city + state + "\n" + self.postalcode + "\n"
         return Address
 
-OID = "119686380"
-O = Order(OID)
-print(O.country)
-print(O.getAddress())
+def getAwaitingOrders():
+    everything = getSSResponse(url_awaitingshipment)
+    orderIds = list()
+    for i in range(0, len(everything['orders'])):
+        orderIds.append(everything['orders'][i]['orderId'])
+    return orderIds
+
+
+myorderIds = getAwaitingOrders()
+
+for orderID in myorderIds:
+    O = Order(str(orderID))
+    print(O.getAddress())
+
+
 
